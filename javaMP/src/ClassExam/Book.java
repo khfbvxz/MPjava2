7ac2997d99d0581807b77de70d9ec0aeb0253b39
YuHwan Kim <khfbvxz@naver.com>
2021-10-02 17:35:21 +0900

    this , this 사용시 주의할점, 객체 치환시 주의할점
     * 사용시 주의할 점
     * this()는 반드시 생성자 코드에서만 호출 할 수 있다.
     * this()는 반드시 같은 클래스 내 생성자를 호출할 때 사용
     * this()는 반드시 생서자의 첫번재 문장이 되어야 한다.!!

--- javaMP/src/ClassExam/Book.java
+++ javaMP/src/ClassExam/Book.java
@@ -1,24 +1,52 @@
 package ClassExam;
-
+/*
+ * this 레퍼런스
+ * this 의 기초 개념 : this 는 객체 자신의 대한 레퍼런스 현재 실행되고 있는 메소드가 속한 객체에 대한 레퍼런스
+ * 
+ * 필요성
+ * public Circle(int radius) { radius = radius} 둘다 모두 매개변수 radius에 접근  
+ * public Circle(int radius) { this.radius = radius} 
+ *                             멤버 radius     매개변수 radius
+ *                             
+ * 사용시 주의할 점
+ * this()는 반드시 생성자 코드에서만 호출 할 수 있다.
+ * this()는 반드시 같은 클래스 내 생성자를 호출할 때 사용
+ * this()는 반드시 생서자의 첫번재 문장이 되어야 한다.!!
+ */
 public class Book {
 	String title;
 	String author;
 	
+	void show() { System.out.println(title + " " + author);}
+	// this 로 다른 생성자 호출
+	public Book() {
+//		System.out.println("생성자 호출됨~");  // 주의할점 3번째 
+		this("","");
+		System.out.println("생성자 호출됨~");  // 주의할점 3 
+	}
 	public Book(String t) {// 생성자 
-		title = t ;
-		author = "작자 미상";
+		this(t,"작자미상");
+		
 	}
 	public Book(String t,String a) {// 생성자 
-		title = t;
-		author = a;
+		this.title = t;
+		this.author = a;
+		
 	}
 	
 	public static void main(String[] args) {
 		Book littlePrince = new Book("어린왕자","생택쥐페리");
-		// 생성자 public Book(String t,String a)호출
-		Book loveStory = new Book("춘향전"); //생성자 public Book(String t) 호출
-		System.out.println(littlePrince.title + "  " + littlePrince.author);
-		System.out.println(loveStory.title + "  " + loveStory.author);
+		Book loveStory = new Book("춘향전");
+		Book emptyBook = new Book();
+		littlePrince.show();
+		loveStory.show();
+		emptyBook.show();
+//		Book littlePrince = new Book("어린왕자","생택쥐페리");
+//		// 생성자 public Book(String t,String a)호출
+//		Book loveStory = new Book("춘향전"); //생성자 public Book(String t) 호출
+		
+//		System.out.println(littlePrince.title + "  " + littlePrince.author);
+//		System.out.println(loveStory.title + "  " + loveStory.author);
 		
 	}
 }
--- javaMP/src/ClassExam/Circle.java
+++ javaMP/src/ClassExam/Circle.java
@@ -2,35 +2,54 @@ package ClassExam;
 
 public class Circle {
 // 접근권한 클래스선언 클래스명
-	public int radius ; // 원의 반지름 필드    필드(변수)
-	public String name; // 원의 이름 필드
+//	public int radius ; // 원의 반지름 필드    필드(변수)
+//	public String name; // 원의 이름 필드
+//	
+//	// 생성자의 이름은 클래스의 이름과 같게  
+//	// 생성자는 여러개 작성(오버로딩) 가능
+//	// 생성자에 리턴타입 지정할수 없음!!!  void를 리턴타입으로 사용할 수 없읍!!
+//	// public void Circle() {...} 
+//	
+//	//  생성자의 목적!!!
+//	// 객체 생성될때 필요한 초기작업을 위함!!!!
+//	public Circle() { // 매개변수 없는 생성자
+//		radius=1; name =""; // 필드 초기화
+//	}
+//	public Circle(int r, String n) { 
+//		radius =r ; name = n ;
+//	}
+//	public double getArea() {// 원의 면적 계산 메소드
+//		return 3.14*radius*radius;
+//	}
 	
-	// 생성자의 이름은 클래스의 이름과 같게  
-	// 생성자는 여러개 작성(오버로딩) 가능
-	// 생성자에 리턴타입 지정할수 없음!!!  void를 리턴타입으로 사용할 수 없읍!!
-	// public void Circle() {...} 
-	
-	//  생성자의 목적!!!
-	// 객체 생성될때 필요한 초기작업을 위함!!!!
-	public Circle() { // 매개변수 없는 생성자
-		radius=1; name =""; // 필드 초기화
-	}
-	public Circle(int r, String n) { 
-		radius =r ; name = n ;
-	}
-	public double getArea() {// 원의 면적 계산 메소드
-		return 3.14*radius*radius;
-	}
+	int radius; 
+	public Circle(int radius) {this.radius = radius;}
+	public void set(int radius) {this.radius=radius;}
 	
 	public static void main(String[] args) {
-		Circle pizza = new Circle(10,"자바피자");    // Circle 객체 생성 반지름 10 , 이름 자바피자로 초기화
-		// 생성자는 new 를 통해 객체를 생성할 때 한번만 호출!!!
-		double area = pizza.getArea();
-		System.out.println(pizza.name + "의 면적은 "+ area);
+		Circle ob1 = new Circle(1);
+		Circle ob2 = new Circle(2);
+//		Circle ob3 = new Circle(3);
+		Circle s;
+		// ## 객체 치횐 시 주의 할 점
+		s = ob2;
+		ob1 = ob2 ;
+	
+//		ob1.set(4);
+//		ob2.set(5);
+//		ob3.set(6);
+		System.out.println("ob1.radius>>" + ob1.radius);
+		System.out.println("ob2.radius>>" + ob2.radius);
+//		System.out.println("ob3.radius>>" + ob3.radius);
 		
-		Circle donut = new Circle(); // 객체 생성 매개변수 없으므로 1, "" 으로 초기화
-		donut.name = "던킨도넛"; // 이름 변경 
-		double darea = donut.getArea();
-		System.out.println(donut.name + "의 면적은 "+ darea);
+//		Circle pizza = new Circle(10,"자바피자");    // Circle 객체 생성 반지름 10 , 이름 자바피자로 초기화
+//		// 생성자는 new 를 통해 객체를 생성할 때 한번만 호출!!!
+//		double area = pizza.getArea();
+//		System.out.println(pizza.name + "의 면적은 "+ area);
+//		
+//		Circle donut = new Circle(); // 객체 생성 매개변수 없으므로 1, "" 으로 초기화
+//		donut.name = "던킨도넛"; // 이름 변경 
+//		double darea = donut.getArea();
+//		System.out.println(donut.name + "의 면적은 "+ darea);
 	}
 }
