da5c0e699e71447cb2a72fbf2ad70226c3c32f8d
YuHwan Kim <khfbvxz@naver.com>
2021-10-03 14:15:12 +0900

    클래스 상속과 함께 인터페이스 구현

--- javaMP/src/Inheritance/InterFaceEx.java
+++ javaMP/src/Inheritance/InterFaceEx.java
@@ -8,27 +8,69 @@ interface PhoneInterface{    // 인터페이스 선언
 		System.out.println("** Phone **");
 	}
 }
+interface MobilePhoneInterface extends PhoneInterface{
+	void sendSMS();//추상 메소드
+	void receiveSMS();// 추상 메소드 
+}
+interface MP3Interface extends PhoneInterface{
+	void play();//추상 메소드
+	void stop();// 추상 메소드 
+}
+class PDA{// 클래스 작성
+	public int calculate(int x, int y) {
+		return x+y;
+	}
+}
+interface MusicPhoneInterface extends MobilePhoneInterface,MP3Interface{ // 다중 상속
+	void playMP3RingTone();//추상 메소드
+	
+}
 
-class SamsungPhone implements PhoneInterface{ // 인터페이스 구현
-	// 모든 추상메소드 구현
+interface AIInterface{
+	void recognizeSpeech();//음성인식
+	void systhesizeSpeech();//음성 합성
+}
+class SmartPhone extends PDA implements MobilePhoneInterface, MP3Interface{ // 인터페이스 구현
+	// 모바일 추상메소드 구현
 	@Override
 	public void sendCall() {
 		System.out.println("띠ㄹㄹㄹㄹㄹㄹ");
 	}
+	@Override
 	public void receiveCall() {
 		System.out.println("전화왔어요~~");
 	}
+	@Override
+	public void sendSMS() {
+		System.out.println("문자가요~");
+	}
+	@Override
+	public void receiveSMS() {
+		System.out.println("문자와요~");
+	}
+	
+	/// mp3 추상 메소드 구현
+	@Override
+	public void play() {
+		System.out.println("음악 스타트~");
+	}
+	@Override
+	public void stop() {
+		System.out.println("음악 멈춰~~");
+	}
 	// 메소드 추가 작성
-	public void flash() {System.out.println(" 전화기에 불이 켜졌네~");}
+	public void schedule() {System.out.println("일정 관리 합니다");}
 	
 }
 
 public class InterFaceEx {
 	public static void main(String[] args) {
-		SamsungPhone phone = new SamsungPhone();
+		SmartPhone phone = new SmartPhone();
 		phone.printLogo();
 		phone.sendCall();
-		phone.receiveCall();
-		phone.flash();
+//		phone.receiveCall();
+		phone.play();
+		System.out.println("3 + 5 = ? >>" + phone.calculate(3, 5));
+		phone.schedule();
 	}
 }
