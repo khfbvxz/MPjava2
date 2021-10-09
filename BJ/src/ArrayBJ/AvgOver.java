63457464dcb1ff42ec3f810ab44d39f11fa21290
YuHwan Kim <khfbvxz@naver.com>
2021-10-09 22:13:34 +0900

    ,

--- BJ/src/ArrayBJ/AvgOver.java
+++ BJ/src/ArrayBJ/AvgOver.java
@@ -1,10 +1,22 @@
 package ArrayBJ;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class AvgOver {
 	public static void main(String[] args) {
+		System.out.println("hello");
 		Scanner sc = new Scanner(System.in);
-		
-	}
+		int c = sc.nextInt();
+		int a = 0;
+		int[][] arr = new int[c][];
+		for(int i = 0 ; i<c ; i++) {
+			a = sc.nextInt();
+			arr[i][0]=a;
+			for(int j = 0 ; j<a;j++) {
+				arr[i][j+1] = sc.nextInt();
+			}
+		}//for end
+		System.out.println(Arrays.deepToString(arr));
+	}//main end
 }
