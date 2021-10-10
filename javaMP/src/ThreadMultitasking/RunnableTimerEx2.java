b0ad45aa45d575095374eb828c4cd4d0904a3d43
YuHwan Kim <khfbvxz@naver.com>
2021-10-10 23:03:20 +0900

    Runnable 인터페이스 사용

--- javaMP/src/ThreadMultitasking/RunnableTimerEx2.java
+++ javaMP/src/ThreadMultitasking/RunnableTimerEx2.java
@@ -3,10 +3,10 @@ package ThreadMultitasking;
 import java.awt.*;
 import javax.swing.*;
 
-class TimerThread extends Thread { // Thread 상속 받음
+class TimerRunnable implements Runnable  { // Thread 상속 받음
 	private JLabel timerLabel; // 타이머 값이 출력되는 레이블
 	
-	public TimerThread(JLabel timerLabel) {
+	public TimerRunnable(JLabel timerLabel) {
 		this.timerLabel = timerLabel; // 타이머 카운트를th.start();/ 출력하는 레이블
 	}
 	// 스레드 코드 . run() 이 종료되면 스레드 종료
@@ -19,7 +19,7 @@ class TimerThread extends Thread { // Thread 상속 받음
 //			System.out.println(n);  // 레이블에 카운트 값 출력
 			n++; //  카운트 증가
 			try {
-				sleep(1000);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 				return;  // 예외가 발생하면 스레드 종료
 			}
@@ -27,9 +27,9 @@ class TimerThread extends Thread { // Thread 상속 받음
 	}
 }
 
-public class ThreadTimerEx extends JFrame{
-	public ThreadTimerEx() {
-		setTitle("스레드를 상속받은 타이머 스레드 예제");
+public class RunnableTimerEx2 extends JFrame{
+	public RunnableTimerEx2() {
+		setTitle("Runnable을 구현한  타이머 스레드 예제");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Container c = getContentPane();
 		c.setLayout(new FlowLayout());
@@ -39,13 +39,14 @@ public class ThreadTimerEx extends JFrame{
 		timerLabel.setFont(new Font("Gothic", Font.ITALIC , 80));
 		c.add(timerLabel);// 레이블에 컨텐트 팬에 부착
 		
-		TimerThread th = new TimerThread(timerLabel);
-		// 타이머 스레드 객체 생성. 타이머 값을 출력할 레이블에 생성자에 전달.
-		setSize(300,170);
+		// 타이머 스레드로 사용할 Runnable 객체 생성 . 타이머 값을 출력할 레이블을 생성자에 전달 
+		TimerRunnable runa = new TimerRunnable(timerLabel);
+		Thread th = new Thread(runa);
+		setSize(250,150);
 		setVisible(true);
 		th.start(); // 타이머 스레드의 실행을 시작하게 한다. 
 	}
 	public static void main(String[] args) {
-		new ThreadTimerEx();
+		new RunnableTimerEx2();
 	}
 }
---
+++ javaMP/src/ThreadMultitasking/TestRunnable.java
@@ -0,0 +1,25 @@
+package ThreadMultitasking;
+
+//class TimerRunnable implements Runnable { // Thread 상속 받음
+//	int n = 0;
+//
+//	@Override
+//	public void run() {
+//		while (true) {
+//			System.out.println(n);
+//			n++;
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				return;
+//			}
+//		}
+//	}
+//}
+
+public class TestRunnable {
+	public static void main(String[] args) {
+//		Thread th = new Thread(new TimerRunnable()); // 스레드 객체 생성
+//		th.start();// 스레드 시작
+	}
+}
