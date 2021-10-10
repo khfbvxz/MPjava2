a8c3076db96ec6786400501298b1bc0fa89504f6
YuHwan Kim <khfbvxz@naver.com>
2021-10-10 22:49:59 +0900

    타이머 스레드

---
+++ javaMP/src/ThreadMultitasking/TestThread.java
@@ -0,0 +1,25 @@
+package ThreadMultitasking;
+
+//class TimerThread extends Thread { // Thread 상속 받음
+//	int n = 0;
+//
+//	@Override
+//	public void run() {
+//		while (true) {
+//			System.out.println(n);
+//			n++;
+//			try {
+//				sleep(1000);
+//			} catch (InterruptedException e) {
+//				return;
+//			}
+//		}
+//	}
+//}
+
+public class TestThread {
+	public static void main(String[] args) {
+//		TimerThread th = new TimerThread(); // 스레드 객체 생성
+//		th.start();// 스레드 시작
+	}
+}
